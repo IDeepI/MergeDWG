74810404bbb8586f0aca5d6b9e80294a1b58f49d
Игорь Соколов <strelok111@gmail.com>
2020-09-21 14:57:38 +0300

    "stdole"

--- MergeDWG/Program.cs
+++ MergeDWG/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Configuration;
 using System.IO;
 using System.Text;
 //using AutoCAD;
@@ -13,29 +12,61 @@ namespace MergeDWG
         static string mergedFile;
         static void Main(string[] args)
         {
-           /* 
-            string[] tmpargs = new string[4];
-            tmpargs[0] = "d:\\Work\\DWG\\ПП-825_Каховская\\ПП825\\ВИЛЕ.674791.000ПЭ3__1.dwg";
-            tmpargs[1] = "d:\\Work\\DWG\\ПП-825_Каховская\\ПП825\\ВИЛЕ.674791.000Э3__1.dwg";
-            tmpargs[2] = "d:\\Work\\DWG\\ПП-825_Каховская\\ПП825\\ВИЛЕ.674791.000Э3__2.dwg";
-            tmpargs[3] = "d:\\Work\\DWG\\ПП-825_Каховская\\ПП825\\ВИЛЕ.674791.000Э3__3.dwg";
-           */
-            
-            CopyDWGTemplate(args[0]);
-            CreateLispCode(args);
-            CreateLispFile();
 
-            Process process =  Process.Start(mergedFile);
-            //var Acad = new AutoCAD.AcadApplication();
-            //Acad.ActiveDocument.SendCommand("ii");
+            if (args.Length == 0)
+            {
+                string input;
+
+                Settings.Default.lispFilePath = Settings.Default.lispFilePath;
+                Settings.Default.templateFilePath = Settings.Default.templateFilePath;
+                Settings.Default.deltaX = Settings.Default.deltaX;
+
+                Console.WriteLine($"Аргументов { args.Length }");
+
+                Console.Write("lispFilePath - ");
+                input = Console.ReadLine();
+                if (input != "")
+                {
+                    Settings.Default.lispFilePath = input;
+                }
+
+                Console.Write("templateFilePath - ");
+                input = Console.ReadLine();
+                if (input != "")
+                {
+                    Settings.Default.templateFilePath = input;
+                }
+
+
+                Console.Write("deltaX - ");
+                input = Console.ReadLine();
+                if (input != "")
+                {
+                    Int32.TryParse(input, out int deltaX);
+                    Settings.Default.deltaX = deltaX;
+                }
+
+                Settings.Default.Save();
+            }
+            else
+            {
+                CopyDWGTemplate(args[0]);
+                CreateLispCode(args);
+                CreateLispFile();
+
+                Process process = Process.Start(mergedFile);
+                //var Acad = new AutoCAD.AcadApplication();
+                //Acad.ActiveDocument.SendCommand("ii");
+            }
+
         }
 
         static void CreateLispFile()
         {
-            string lispFilePath = ConfigurationManager.AppSettings["lispFilePath"];
-
             try
             {
+                string lispFilePath = Settings.Default.lispFilePath;
+
                 using (StreamWriter sw = new StreamWriter(lispFilePath, false, System.Text.Encoding.Default))
                 {
                     sw.WriteLine(lispCode);
@@ -45,18 +76,19 @@ namespace MergeDWG
             catch (Exception e)
             {
                 Console.WriteLine("The process failed: {0}", e.ToString());
+                Console.ReadKey();
             }
         }
 
         static void CopyDWGTemplate(string fPath)
         {
-            string templateFilePath = ConfigurationManager.AppSettings["templateFilePath"];
-            string mergedFilePath = Path.GetDirectoryName(fPath);
-            string mergedFileName = Path.GetFileName(fPath).Substring(0, 15) + ".dwg";
-            mergedFile = Path.Combine(mergedFilePath, mergedFileName);
-
             try
             {
+                string templateFilePath = Settings.Default.templateFilePath;
+                string mergedFilePath = Path.GetDirectoryName(fPath);
+                string mergedFileName = Path.GetFileName(fPath).Substring(0, 15) + ".dwg";
+                mergedFile = Path.Combine(mergedFilePath, mergedFileName);
+
                 // Use the Path.Combine method to safely append the file name to the path.
                 // Will overwrite if the destination file already exists.
                 File.Copy(templateFilePath, mergedFile, true);
@@ -65,30 +97,38 @@ namespace MergeDWG
             catch (DirectoryNotFoundException dirNotFound)
             {
                 Console.WriteLine(dirNotFound.Message);
+                Console.ReadKey();
             }
         }
 
         static void CreateLispCode(string[] filePaths)
         {
-            lispCode.Append("(defun c:ii() ");
-
-            string fileName;
-            int xPos = 0;
-            int deltaX = 0;
-            // Сдвиг по оси x для следующего чертежа
-            int.TryParse(ConfigurationManager.AppSettings["deltaX"], out deltaX);
-
-            foreach (var filePath in filePaths)
+            try
             {
-                fileName = Path.GetFileName(filePath);
+                lispCode.Append("(defun c:ii() ");
 
-                lispCode.Append("(command\"_insert\"\"" + fileName + "\"\"МАСШТАБ\"\"1\"\"ПОВОРОТ\"\"0\"\"" + xPos + ",0\")");
-                
-                xPos += deltaX; 
+                string fileName;
+                int xPos = 0;
+
+                // Сдвиг по оси x для следующего чертежа
+                int deltaX = Settings.Default.deltaX;
+
+                foreach (var filePath in filePaths)
+                {
+                    fileName = Path.GetFileName(filePath);
+
+                    lispCode.Append("(command\"_insert\"\"" + fileName + "\"\"МАСШТАБ\"\"1\"\"ПОВОРОТ\"\"0\"\"" + xPos + ",0\")");
+
+                    xPos += deltaX;
+                }
+
+                lispCode.Append(")");
             }
-
-            lispCode.Append(")");
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+                Console.ReadKey();
+            }
         }
     }
-
 }
